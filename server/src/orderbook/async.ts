0fad5bfd4cc14dbf28ca5231447da4ea5f32a599
Geo Angelopoulos <el10664@mail.ntua.gr>
2021-09-11 22:47:01 +0300

    async JSdoc

--- server/src/orderbook/async.ts
+++ server/src/orderbook/async.ts
@@ -2,9 +2,16 @@ import { Mutex } from 'async-mutex';
 import { HandleOrder, Order } from '.';
 import { Execution } from './orders';
 
-const mutex = new Mutex(); // creates a shared mutex instance
+const mutex = new Mutex();
+
+/**
+ * @function HandleOrderAsync
+ * @param {Order} order 
+ * @returns {Promise<[boolean, Execution]>}
+ */
 
 const HandleOrderAsync = async (order: Order): Promise<[boolean, Execution]> => {
+	// wait to aquire mutex
 	const release = await mutex.acquire();
 	try {
 		return new Promise(resolve => resolve(HandleOrder(order)));
