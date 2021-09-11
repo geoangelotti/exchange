c2666ec6f1959eeb40409a7b53a14c68aa2662c4
Geo Angelopoulos <el10664@mail.ntua.gr>
2021-09-11 15:39:11 +0300

    simpler orderbook

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -6,15 +6,11 @@ import Orderbook from "../components/orderbook"
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Orderbook />
+    <Seo title="Orderbook" />
     <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      <Link to="/transactions/">Go to transaction history</Link> <br />
     </p>
+    <Orderbook />
   </Layout>
 )
 
