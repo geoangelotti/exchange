5e4fc70527176b1e004c1f18a33aeeed9da28854
Geo Angelopoulos <el10664@mail.ntua.gr>
2021-09-11 22:33:31 +0300

    utilities jsdoc

--- server/src/router/utlis.ts
+++ server/src/router/utlis.ts
@@ -1,17 +1,36 @@
 import { Application, Request, Response, NextFunction } from 'express';
 import { vars } from '../env';
 
+/**
+ * @type {Wrapper}
+ */
+
 type Wrapper = (app: Application) => void;
+
+/**
+ * @function applyMiddleware
+ * @param {Application} app
+ * @param {Wrapper[]} middlewareWrappers
+ */
 
 const applyMiddleware = (app: Application, middlewareWrappers: Wrapper[]) => {
 	middlewareWrappers.forEach(wrapper => wrapper(app));
 };
 
+/**
+ * @type {Handler}
+ * express handle route function
+ */
+
 type Handler = (
 	req: Request,
 	res: Response,
 	next: NextFunction
-) => Promise<void> | void;
+) => Promise<void>;
+
+/**
+ * @type {Route}
+ */
 
 type Route = {
 	path: string;
@@ -19,19 +38,39 @@ type Route = {
 	handler: Handler | Handler[];
 };
 
-const catchErrors = (fn: any) => (
+/**
+ * @function catchErrors passes errors to @type {NextFunction} for default express error handling
+ * @param {Handler| Handler[]} fn
+ * @returns {Promise<void | Handler>}
+ */
+
+const catchErrors = (fn: Handler | Handler[]) => (
 	req: Request,
 	res: Response,
 	next: NextFunction
-) => fn(req, res, next).catch(next);
+) =>
+	Array.isArray(fn)
+		? Promise.any(fn).catch(next)
+		: fn(req, res, next).catch(next);
 
-const applyRoutes = (router: Application, routes: Route[]) => {
+/**
+ * @function applyRoutes
+ * @param {Application} app
+ * @param {Route[]}routes
+ */
+
+const applyRoutes = (app: Application, routes: Route[]) => {
 	routes.forEach(route => {
 		const { method, path, handler } = route;
 		// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-		(<any>router)[method](path, catchErrors(handler));
+		(<any>app)[method](path, catchErrors(handler));
 	});
 };
+
+/**
+ * @function serve
+ * @param {Application} app
+ */
 
 const serve = (app: Application) => {
 	const { port, nodeEnv } = vars;
