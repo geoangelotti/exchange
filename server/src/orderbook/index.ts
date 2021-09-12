f3a7d071fcef9a8ee887855190eaf66c286cf0fc
Geo Angelopoulos <el10664@mail.ntua.gr>
2021-09-12 19:27:40 +0300

    orderbook comments

--- server/src/orderbook/index.ts
+++ server/src/orderbook/index.ts
@@ -5,6 +5,10 @@ import {
 import { Execution, LimitExecution, MarketExecution } from './orders';
 import { getLastTransaction } from './transactions';
 
+/**
+ * @typedef {MarketOrder}
+ */
+
 interface MarketOrder {
 	uuid: string;
 	side: 'buy' | 'sell';
@@ -12,6 +16,10 @@ interface MarketOrder {
 	quantity: string;
 	timestamp: Date;
 }
+
+/**
+ * @typedef {LimitOrder}
+ */
 
 interface LimitOrder {
 	uuid: string;
@@ -22,13 +30,24 @@ interface LimitOrder {
 	timestamp: Date;
 }
 
+/**
+ * @typedef {Order}
+ */
+
 type Order = MarketOrder | LimitOrder;
 
-/*	Minimum Priority Queue
- *	Selling occurs at the smallest price (as far as a buyer is concerned)
- *	Efficient way to peak the largest LimitOrder O(1)
- *	Efficient way to get the smallest LimitOrder O(logn)
- *	Efficient way to place the order to the appropriate place O(logn)
+/**
+ * @typedef {PriorityQueue}
+ */
+
+type PriorityQueue = MinPriorityQueue<LimitOrder>|MaxPriorityQueue<LimitOrder>;
+
+/** 
+ * Minimum Priority Queue
+ * Selling occurs at the smallest price (as far as a buyer is concerned)
+ * Efficient way to peak the smallest LimitOrder O(1)
+ * Efficient way to get the smallest LimitOrder O(log(n))
+ * Efficient way to place the order to the appropriate place O(log(n))
  */
 
 const SellQueue = new MinPriorityQueue<LimitOrder>({
@@ -39,11 +58,12 @@ const SellQueue = new MinPriorityQueue<LimitOrder>({
 	},
 });
 
-/*	Maximum Priority Queue
- *	Selling occurs at the largest price (as far as a seller is concerned)
- *	Efficient way to peak the largest LimitOrder O(1)
- *	Efficient way to get the largest LimitOrder O(logn)
- *	Efficient way to place the order to the appropriate place O(logn)
+/**
+ * Maximum Priority Queue
+ * Selling occurs at the largest price (as far as a seller is concerned)
+ * Efficient way to peak the largest LimitOrder O(1)
+ * Efficient way to get the largest LimitOrder O(log(n))
+ * Efficient way to place the order to the appropriate place O(log(n))
  */
 
 const BuyQueue = new MaxPriorityQueue<LimitOrder>({
@@ -54,32 +74,40 @@ const BuyQueue = new MaxPriorityQueue<LimitOrder>({
 	},
 });
 
+/**
+ * @function arrangeQueueStash
+ * @param {'sell' | 'buy'} side 
+ * @returns {[PriorityQueue, PriorityQueue]} 
+ */
+
+const arrangeQueueStash = (side: 'buy' | 'sell'): [PriorityQueue, PriorityQueue] =>
+	side === 'buy' ? [SellQueue, BuyQueue] : [BuyQueue, SellQueue]
+
+/**
+ * @function HandleOrder
+ * Execute the Order
+ * If partial or no execution of the order the remainder becomes a limit order
+ * @param {Order} order 
+ * @returns {[boolean, Execution]}
+ */
+
 const HandleOrder = (order: Order): [boolean, Execution] => {
-	let Queue:
-		| MinPriorityQueue<LimitOrder>
-		| MaxPriorityQueue<LimitOrder> = SellQueue;
-	let Stash:
-		| MinPriorityQueue<LimitOrder>
-		| MaxPriorityQueue<LimitOrder> = BuyQueue;
-	if (order.side === 'sell') {
-		const temp = Queue;
-		Queue = Stash;
-		Stash = temp;
-	}
+	const [Queue, Stash] = arrangeQueueStash(order.side);
 	const execution =
 		order.type === 'market'
 			? MarketExecution(order, Queue)
 			: LimitExecution(order, Queue);
 	const [result, , remainderOrder] = execution;
-	// Partial or no execution of the Order
-	// The remainder of the Order needs to be added as a Market Order to the Appropriate stash
+	
+	// if partial or no execution of the Order
+	// The remainder of the Order needs to be added as a Limit Order to the stash
 	if (result !== 'full') {
 		if (order.type === 'limit') {
 			Stash.enqueue(remainderOrder as LimitOrder);
 		} else {
 			// Market Order needs to be transformed to Limit Order
 			// the price is the price of the last recorder transaction
-			// theoritically this does not happen as the exchange has market makers to provide liquidity to match every incoming Market Order (Market Maker)
+			// theoritically this does not happen as the exchange has market makers to provide adequate liquidity
 			const lastTransaction = getLastTransaction();
 			if (lastTransaction) {
 				Stash.enqueue({
@@ -98,4 +126,4 @@ const HandleOrder = (order: Order): [boolean, Execution] => {
 	return [true, execution];
 };
 
-export { SellQueue, BuyQueue, LimitOrder, MarketOrder, Order, HandleOrder };
+export { SellQueue, BuyQueue, LimitOrder, MarketOrder, Order, PriorityQueue, HandleOrder };
