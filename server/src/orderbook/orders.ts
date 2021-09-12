c2e878da0bd6022cafca20a449fe4c508c267e0e
Geo Angelopoulos <el10664@mail.ntua.gr>
2021-09-12 21:28:33 +0300

    order execution comments

--- server/src/orderbook/orders.ts
+++ server/src/orderbook/orders.ts
@@ -1,40 +1,47 @@
-import { MinPriorityQueue, MaxPriorityQueue } from '@datastructures-js/priority-queue';
-import { LimitOrder, MarketOrder } from '.';
+import { LimitOrder, MarketOrder, PriorityQueue } from '.';
 import { recordTransaction, Transaction } from './transactions';
+
+/**
+ * @typedef {Execution} - Order execution outcome
+ */
 
 type Execution = [ 'full' | 'partial' | 'no fill', Array<Transaction>, MarketOrder|LimitOrder];
 
 /**
- * Arrange Seller and Buyer for asker and bidder
+ * @function arrangeSellerBuyer
+ * Arrange Seller and Buyer from asker and bidder depending on the side
  * @param {string} asker 
  * @param {string} bidder 
  * @param {'sell' | 'buy'} side
- * @returns {[string, string]} - Return Seller and Buyer from asker and bidder in the proper order
+ * @returns {[string, string]} - Return Seller and Buyer
  */
 
 const arrangeSellerBuyer = (asker: string, bidder: string, side: 'sell' | 'buy'): [string, string] => side === 'sell' ? [asker, bidder] : [bidder,asker] ;
 
-/**  
- *	Market Order Execution
- *	Try to fullfil the incoming Market Order with the head of the Queue 
- *	Until the Market Order is filled completely or Queue is empty
- *	If head is partially filled then it is enqueued with the remaining quantity
- * 	Record the successfull transactions
- *	@param {MarketOrder} ask - Incoming Market Order
- *	@param {MinPriorityQueue<LimitOrder>|MaxPriorityQueue<LimitOrder>} bookQueue - Priority Queue with the matching Limit Orders
+/** 
+ * @function MarketExecution
+ * Try to fullfil the incoming Market Order with the head of the Queue 
+ * Continue with the next Head of the Queue
+ * Until the Market Order is filled completely or Queue is empty
+ * If head is partially filled then it is enqueued with the remaining quantity
+ * Record the successfull transactions
+ * @param {MarketOrder} ask - Incoming Market Order
+ * @param {PriorityQueue} bookQueue - Priority Queue with the matching Limit Orders
 `* @returns {Execution} - Result of the execution, Transactions executed, Market Order after the execution
  */
 
-const MarketExecution = (ask: MarketOrder, bookQueue: MinPriorityQueue<LimitOrder>|MaxPriorityQueue<LimitOrder>): Execution => {
+const MarketExecution = (ask: MarketOrder, bookQueue: PriorityQueue): Execution => {
 	const transactions: Array<Transaction> = [];
 	let quantity_demand = Number(ask.quantity);
 	// while positive demand and not empty queue
 	while (quantity_demand > 0 && !bookQueue.isEmpty()) {
 		const bid = <LimitOrder>bookQueue.dequeue();
 		const [seller, buyer] = arrangeSellerBuyer(ask.uuid, bid.uuid, ask.side);
+
 		const price = Number(bid.price);
 		const quantity_supply = Number(bid.quantity);
 		const delta = quantity_supply - quantity_demand;
+
 		// More supply than demand
 		// Market Order fullfilled
 		// Remainder of Limit Order needs to be requeued
@@ -42,77 +49,80 @@ const MarketExecution = (ask: MarketOrder, bookQueue: MinPriorityQueue<LimitOrde
 			const transaction = {seller, buyer, timestamp: new Date(), price: price.toFixed(4), quantity: quantity_demand.toFixed(4)}
 			recordTransaction(transaction);
 			transactions.push(transaction);
+			
 			// Market Order was filled
+			// Add to the queue the remainder of the transaction
 			quantity_demand = 0;
-			// add to the queue the remainder of the transaction
 			bookQueue.enqueue({...bid, quantity: delta.toFixed(4)});
 		} else {
 			// More demand than supply
 			// Limit Order exhausted
-			// Market Order change demanded quantity
+			// Change demanded quantity of Market Order
 			const transaction = {seller, buyer, timestamp: new Date(), price: price.toFixed(4), quantity: quantity_supply.toFixed(4)}
 			recordTransaction(transaction);
 			transactions.push(transaction);
+
 			quantity_demand = delta * (-1);
 		}
-		// loop back if positive demand and not empty queue
 	}
+
 	if (transactions.length === 0) {
-		// No transaction executed
+		// No Order executed
 		return ['no fill', transactions, ask];
 	} else if (quantity_demand > 0) {
-		// Partial transaction executed
-		// BookQueue exhausted
+		// Partial Order executed
 		return ['partial', transactions, {...ask, quantity: quantity_demand.toFixed(4)}]; 
 	} else {
-		// Full transaction executed
+		// Full Order executed
 		return ['full', transactions, ask];
 	}
 };
 
 /** 
- *  Price Covered 
- *	if current price covers the limit
- *	for buy limit > i
- *	for sell i > 0
- *  @param {number} i - current price
- *  @param {number} limit - price limit
- *	@param {'buy' | 'sell'} side - kind of order performed
- *  @returns {boolean} - if current price "under" price limit
+ * @function priceCovered
+ * if current price covers the limit
+ * for buy: limit > i
+ * for sell: i > 0
+ * @param {number} price - current price
+ * @param {number} limit - price limit
+ * @param {'buy' | 'sell'} side - kind of order performed
+ * @returns {boolean} - if current price covers price limit
  */
 
-const priceCovered = (i: number, limit: number, side: 'buy' | 'sell'): boolean => {
-	const check = limit - i > 0;
+const limitCovered = (price: number, limit: number, side: 'buy' | 'sell'): boolean => {
+	const check = limit - price > 0;
 	return side === 'sell' ? !check : check ;
 }
 
 /**
- *	Limit Order Execution
- *	Try to fullfil the incoming Limit Order with the head of the Queue 
- *	Until the Limit Order is filled completely or Queue is empty or the head's price "over" price limit
- *	If head is partially filled then it is enqueued with the remaining quantity
- * 	Record the successfull transactions
- *	@param {LimitOrder} ask - Incoming Limit Order 
- *	@param {MinPriorityQueue<LimitOrder>|MaxPriorityQueue<LimitOrder>} bookQueue - Priority Queue with the matching Limit Orders
-`* 	@returns {Execution} - Result of the execution, Transactions executed, Limit Order after the execution
+ * @function LimitExecution
+ * Try to fullfil the incoming Limit Order with the head of the Queue
+ * Continue with the next Head of the Queue
+ * Until the Limit Order is filled completely or Queue is empty or the head's price does not cover price limit
+ * If head is partially filled then it is enqueued with the remaining quantity
+ * Record the successfull transactions
+ * @param {LimitOrder} ask - Incoming Limit Order 
+ * @param {PriorityQueue} bookQueue - Priority Queue with the matching Limit Orders
+`* @returns {Execution} - Result of the execution, Transactions executed, Limit Order after the execution
  */
 
-const LimitExecution = (ask: LimitOrder, bookQueue: MinPriorityQueue<LimitOrder>|MaxPriorityQueue<LimitOrder>): Execution => {
+const LimitExecution = (ask: LimitOrder, bookQueue: PriorityQueue): Execution => {
 	const transactions: Array<Transaction> = [];
 	const price_limit = Number(ask.price);
 	let quantity_limit = Number(ask.quantity);
-	// flag if in price
-	let inPrice = true;
-	// while quantity limit not fullfilled and in price range and not empty queue
-	while(quantity_limit > 0 && inPrice && !bookQueue.isEmpty()) {
+	// while quantity limit not fullfilled and limit covered and not empty queue
+	while(quantity_limit > 0 &&  !bookQueue.isEmpty()) {
 		// peak not get head of the Queue
 		const bid = <LimitOrder>bookQueue.front();
+
 		const price_fill =  Number(bid.price);
 		const quantity_fill = Number(bid.quantity);
 		const [seller, buyer] = arrangeSellerBuyer(ask.uuid, bid.uuid, ask.side);
+		
 		// if current price covers the limit
-		if (priceCovered(price_fill, price_limit, ask.side)) {
+		if (limitCovered(price_fill, price_limit, ask.side)) {
 			const delta = quantity_fill - quantity_limit;
+			
 			// More supply than demand
 			// Limit Order fullfilled
 			// Remainder of Limit Order needs to be requeued
@@ -120,44 +130,37 @@ const LimitExecution = (ask: LimitOrder, bookQueue: MinPriorityQueue<LimitOrder>
 				const transaction = {seller, buyer, timestamp: new Date(), price: price_fill.toFixed(4), quantity: quantity_limit.toFixed(4)}
 				recordTransaction(transaction);
 				transactions.push(transaction);
-				// filled the limit order
+
+				// limit order was filled
+				// remove bid from Queue and enqueue the remainder of the head
 				quantity_limit = 0;
-				// remove bid from queue
 				bookQueue.dequeue();
-				// enqueue remainder limit order
 				bookQueue.enqueue({...bid, quantity: delta.toFixed(4)});
 			} else {
 				// More demand than supply
-				// Queued Limit Order exhausted
-				// Incoming Limit Order change demanded quantity
+				// bid exhausted and removed from Queue
+				// Change demanded quantity of Limit Order
 				const transaction = {seller, buyer, timestamp: new Date(), price: price_fill.toFixed(4), quantity: quantity_fill.toFixed(4)}
 				recordTransaction(transaction);
 				transactions.push(transaction);
-				// remove bid from queue
+
 				bookQueue.dequeue();
 				quantity_limit = (-1) * delta;
 			}
 		} else {
-			// break the loop
-			inPrice = false;
+			// price does not cover the limit
+			break;
 		}
 	}
+
 	if (transactions.length === 0) {
-		// No transaction executed
+		// No order executed
 		return ['no fill', transactions, ask];
-	} else if (!inPrice) {
-		// Book price does not cross with ask price
-		if (Number(ask.quantity)-quantity_limit>0) {
-			// Partial execution of the Limit Order
-			return ['partial', transactions, {...ask, quantity: quantity_limit.toFixed(4)}];	
-		}
-		// No transaction executed
-		return ['no fill', transactions, {...ask, quantity: quantity_limit.toFixed(4)}];
 	} else if (quantity_limit > 0) {
-		// Partial transaction executed
+		// Partial order executed
 		return ['partial', transactions, {...ask, quantity: quantity_limit.toFixed(4)}];
 	} else {
-		// Full transaction executed
+		// Full order executed
 		return ['full', transactions, ask];
 	}
 };
