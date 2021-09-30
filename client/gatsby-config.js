c0ab01ffc58d38ce6381100ad9083fa3cfae0050
Geo Angelopoulos <14923436+geoangelotti@users.noreply.github.com>
2021-09-30 17:44:31 +0200

    Update gatsby-config.js

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware")
 
 module.exports = {
   siteMetadata: {
-    title: `Trality Exchange`,
+    title: `Exchange`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
